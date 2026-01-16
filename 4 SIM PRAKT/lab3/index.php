0dc4ed23719e3988d8c61136c5b46c1a21fe8f50
psinao4ka <pradedtimura@gmail.com>
2026-01-16 13:00:05 +0300

    lab1 test

--- 4 SIM PRAKT/lab3/index.php
+++ 4 SIM PRAKT/lab3/index.php
@@ -1,2 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>hytale</title>
+</head>
+<body>
+    <form action="" method="get">
+                <input type="text" name="никита" id="">
+                <button type="submit">кликнуть</button>
+
+    </form>
+    
+</body>
+</html>
 <?php
-echo "hello world";
+if (isset($_GET['никита'])){
+// var_dump($_GET['никита']);
+
+$ultrakill=explode(' ',$_GET['никита']);
+UnFunc($ultrakill);
+
+echo implode(' ', $ultrakill);
+
+}
+function UnFunc(&$ultrakill){
+for ($i=0;$i<count($ultrakill);$i++){
+    if(($i % 2)>0){
+        $ultrakill [$i] = strtoupper($ultrakill[$i]);
+    }
+}
+
+}
